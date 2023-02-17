87087f8e50d0213d77f22b405a01ee1c3edede99
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-02-17 11:27:28 +0100

    Actualizo OFW (8.2.2 -> 8.2.6)

--- app/layout/default.layout.php
+++ app/layout/default.layout.php
@@ -4,9 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
 		<title>{{title}}</title>
-		<link type="image/x-icon" href="/favicon.png" rel="icon">
-		<link type="image/x-icon" href="/favicon.png" rel="shortcut icon">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+		<link type="image/x-icon" href="/favicon.ico" rel="icon">
+		<link type="image/x-icon" href="/favicon.ico" rel="shortcut icon">
 	</head>
 
 	<body>
